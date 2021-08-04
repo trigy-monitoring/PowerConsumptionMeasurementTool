5762aab96c450c94d20b41b2ee67b38f32823005
Danilo Pinotti <danilopinotti@hotmail.com>
2021-08-04 15:27:18 -0300

    Read data from serial port

--- measure.py
+++ measure.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import re
 import signal
-import subprocess
 import sys
 import threading
+import serial
 
 x_data = []
 y_data = []
@@ -11,17 +11,12 @@ readed_line_regex = re.compile('^(?P<time>[0-9.]+?) (?P<current>[0-9.]+?)[\t\s\n
 
 figure = plt.figure(figsize=(12, 6), facecolor='#DEDEDE')
 ax = figure.add_subplot(111)
-ax.set_facecolor('#DEDEDE')
-ax.set_xlabel('Seconds')
-ax.set_ylabel('Current (mA)')
-ax.set_title('mA\n')
-
 signal.signal(signal.SIGINT, lambda sig, frame: plt.close('all'))
 
 
-class Tail():
-    def __init__(self, file_name, callback_line=sys.stdout.write):
-        self.file_name = file_name
+class TailSerial():
+    def __init__(self, serial, callback_line=sys.stdout.write):
+        self.serial = serial
         self.callback_line = callback_line
         self.should_stop = False
 
@@ -29,13 +24,8 @@ class Tail():
         self.should_stop = True
 
     def follow(self):
-        command = subprocess.Popen(
-            ['tail', '-n 1', '-f', self.file_name],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-
         while True:
-            line = command.stdout.readline().decode("utf-8")
+            line = serial.readline().decode("utf-8")
             self.callback_line(line)
             if (self.should_stop):
                 break
@@ -62,10 +52,14 @@ def append_data_from_line(line):
 
 
 def plot_chart():
-    plt.cla()
-
-    if (len(x_data) == 0):
+    if (len(x_data) == 0 or len(x_data) % 100 != 0):
         return
+
+    plt.cla()
+    ax.set_facecolor('#DEDEDE')
+    ax.set_xlabel('Seconds')
+    ax.set_ylabel('Current (mA)')
+    ax.set_title('mA\n')
 
     x_last = x_data[-1]
     y_last = y_data[-1]
@@ -75,25 +69,32 @@ def plot_chart():
     plt.draw()
 
 
-def follow_tail_within_thread(path_to_observe):
-    tail = Tail(
-        path_to_observe,
+def plot_data_from_serial(serial):
+    tail = TailSerial(
+        serial,
         lambda line: plot_chart() if append_data_from_line(line) else 0
     )
-    follow_file = lambda x: tail.follow()
-    file_observer_thread = threading.Thread(target=follow_file, args=(1,))
-    file_observer_thread.setDaemon(True)
-    file_observer_thread.start()
-    return file_observer_thread
+
+    follow_serial = lambda x: tail.follow()
+    serial_observer_thread = threading.Thread(target=follow_serial, args=(1,))
+    serial_observer_thread.setDaemon(True)
+    serial_observer_thread.start()
 
 
 if __name__ == "__main__":
     if (len(sys.argv) <= 1):
-        print("[FAIL] Missing path to read.")
+        print("\033[91m [FAIL] Missing serial port")
         sys.exit(-1)
 
-    path_to_observe = sys.argv[1]
+    if (len(sys.argv) <= 2):
+        print(f"\033[91m [FAIL] Missing baudrate for port \"`{sys.argv[1]}`\"")
+        sys.exit(-1)
 
-    follow_tail_within_thread(path_to_observe)
+    serial = serial.Serial(sys.argv[1], sys.argv[2], timeout=5)
+    serial.close()
+    serial.open()
+
+    plot_data_from_serial(serial)
+
     plt.show()
     sys.exit(0)
