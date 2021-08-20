2a6b23bf5ebf4b5c3758919596e7d097791d980f
Danilo Pinotti <danilopinotti@hotmail.com>
2021-08-20 15:32:45 -0300

    Add Recording Time

--- sensor-code/sensor-code.ino
+++ sensor-code/sensor-code.ino
@@ -3,33 +3,39 @@
 
 Adafruit_INA219 ina219(0x40);
 
-#define READING_TIME 90000
+#define RECORDING_FOR_MS 90000
 
 long reads = 0;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {delay(10);}
+
+  while (!Serial) {
+    delay(10);
+  }
+
   if (! ina219.begin()) {
     Serial.println("Failed to find INA219 chip");
     while (1) {
       delay(10);
     }
   }
-
-  printMeasurement();
 }
 
 void loop() {
-  if (millis() <= READING_TIME) {
-    printMeasurement();
+#if RECORDING_FOR_MS == 0
+  performMeasurement();
+#else
+  if (millis() <= RECORDING_FOR_MS) {
+    performMeasurement();
   }
+#endif
 
-  reads++;
   delay(1);
 }
 
-void printMeasurement()
+void performMeasurement()
 {
   Serial.print(String(millis()) + " " + String(ina219.getCurrent_mA()) + " " + String(ina219.getPower_mW()) + "\n");
+  reads++;
 }
